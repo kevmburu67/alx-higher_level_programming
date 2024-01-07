3ab639da26fe7677dec60a9170b07817b66d6e26
Kevin Mburu <kinyuawamburu18@gmail.com>
2024-01-06 23:38:08 -0800

    message2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+from sys import argv
+
+def print_arguments():
+    num_args = len(argv) - 1  # Subtract 1 to exclude the script name from the count
+    print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}{'.' if num_args == 0 else ':'}")
+
+    if num_args > 0:
+        for i in range(1, num_args + 1):
+            print(f"{i}: {argv[i]}")
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print_arguments()
